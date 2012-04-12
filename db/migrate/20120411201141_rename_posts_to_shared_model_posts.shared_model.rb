bbb611cb02c7be609ffccc9fd630f6519d8664cf
David Paniz <davidpaniz@gmail.com>
2012-04-12 16:47:22 -0300

    new version of model

--- db/migrate/20120411201141_rename_posts_to_shared_model_posts.shared_model.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from shared_model (originally 20120411193949)
-class RenamePostsToSharedModelPosts < ActiveRecord::Migration
-  def change
-    rename_table :posts, :shared_model_posts
-  end
-end
